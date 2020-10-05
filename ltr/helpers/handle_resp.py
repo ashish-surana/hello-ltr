06d12639f7564305d0667c49cbcf43cc7c3436e1
Nathan Day <nathancday@gmail.com>
2020-10-05 14:46:29 -0400

    add verbose=False for feature logging

--- ltr/helpers/handle_resp.py
+++ ltr/helpers/handle_resp.py
@@ -1,9 +1,11 @@
 
 
-def resp_msg(msg, resp, throw=True):
-    print('{} [Status: {}]'.format(msg, resp.status_code))
+def resp_msg(msg, resp, throw=True, verbose=False):
+    if verbose:
+        print('{} [Status: {}]'.format(msg, resp.status_code))
     if resp.status_code >= 400:
-        print(resp.text)
+        if verbose:
+            print(resp.text)
         if throw:
             raise RuntimeError(resp.text)
 
